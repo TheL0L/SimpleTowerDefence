11a52d1baddce2b961b141d2d17e2a9e34c8d176
Sergei <sergeiyakima1@gmail.com>
2024-03-15 18:08:58 +0200

    added a gamemanager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int health = 30;
+    private int gold = 100;
+    private int wave_index = -1;
+
+    private List<Vector2> path_points = new List<Vector2>();
+    private List<Wave> waves = new List<Wave>();
+    public List<GameObject> towers = new List<GameObject>();
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public List<Vector2> getPath()
+    {
+        return new List<Vector2>(path_points);
+    }
+
+    public bool isAlive()
+    {
+        return health > 0;
+    }
+
+    public void addGold(int gold)
+    {
+        if (gold > 0) { this.gold += gold; }
+    }
+
+    public int getGold()
+    {
+        return gold;
+    }
+
+    public bool removeGold(int gold)
+    {
+        if (gold < 0) { return false; }
+        if (this.gold < gold) { return false; }
+
+        this.gold -= gold;
+        return true;
+    }
+}
